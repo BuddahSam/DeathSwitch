51bcc233091b98b31ec2825e8bbb700301b58d5a
BuddahSam <buddahsam@gmail.com>
2015-02-19 02:55:51 +0000

    Simple Setup for UI
    Set up the Unity Project and added the basics for the desktopScene.

---
+++ New folder/Assets/Scripts/changeScene.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class changeScene : MonoBehaviour {
+
+	public void QuitGame(string quitTheGame){
+		Application.Quit();
+		Debug.Log("Game has Quit");
+	}
+
+	public void ChangeToScene (string sceneToChangeTo) {
+		Application.LoadLevel (sceneToChangeTo);
+	}
+	
+}
