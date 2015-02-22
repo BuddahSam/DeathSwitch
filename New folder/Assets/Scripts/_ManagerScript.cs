c433871891cf9ab4f392dde6f5e98d51ddee36c5
BuddahSam <buddahsam@gmail.com>
2015-02-22 15:29:52 +0000

    Added in the mob level

--- New folder/Assets/Scripts/_ManagerScript.cs
+++ New folder/Assets/Scripts/_ManagerScript.cs
@@ -1,15 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class changeScene : MonoBehaviour {
+public class _ManagerScript : MonoBehaviour {
 
+
+	// Used to quite game
 	public void QuitGame(string quitTheGame){
 		Application.Quit();
 		Debug.Log("Game has Quit");
 	}
 
+	//Used to change to selected Level
 	public void ChangeToScene (string sceneToChangeTo) {
 		Application.LoadLevel (sceneToChangeTo);
 	}
 	
 }
+
---
+++ New folder/Assets/Scripts/movementScript.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movementScript : MonoBehaviour {
+
+	private Vector3 postition;
+	public float speed;
+	public CharacterController controller;
+
+	public AnimationClip run;
+	public AnimationClip idle;
+	public AnimationClip meleAttack;
+
+
+	// Use this for initialization
+	void Start () {
+		postition = transform.position;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton(0))
+		{
+			//Locate where Player Clicked Terrain
+			locatePosition();
+		}
+
+		//Move the player to position
+		moveToPosition();
+	}
+
+	void locatePosition()
+	{
+		RaycastHit hit;
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+		if (Physics.Raycast (ray, out hit, 1000)) 
+			{
+			postition = new Vector3(hit.point.x, hit.point.y, hit.point.z);
+			//Debug.Log(postition);
+		}
+	}
+
+	void moveToPosition()
+	{
+
+		// when player is moving
+		if (Vector3.Distance (transform.position, postition) > 1) {
+			Quaternion newRotation = Quaternion.LookRotation (postition - transform.position);
+
+			newRotation.x = 0f;
+			newRotation.z = 0f;
+
+			transform.rotation = Quaternion.Slerp (transform.rotation, newRotation, Time.deltaTime * 10);
+			controller.SimpleMove (transform.forward * speed);
+
+			animation.Play(run.name);
+		} 
+
+		//for when player is stationary
+		else
+		{
+			animation.Play(idle.name);
+		}
+	}
+}
